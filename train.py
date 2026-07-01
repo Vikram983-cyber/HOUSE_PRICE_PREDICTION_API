2cae39b1ff65e64b61f1d7e074f4ca98767b62dd
Vikram983-cyber <rajvikram331@gmail.com>
2026-07-02 01:17:45 +0530

    Reduce model size

--- train.py
+++ train.py
@@ -20,7 +20,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y,
 
 #training a model
 model = RandomForestRegressor(
-    n_estimators=100,
+    n_estimators=10,
+    max_depth=8,
     random_state=42
 )
 model.fit(X_train, y_train)
